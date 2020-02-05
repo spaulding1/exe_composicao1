e0d07fff76040ad36b591e6014e93749e3b0e618
unknown <andersonuchoa3@gmail.com>
2020-02-05 14:13:32 -0300

    Continuação do exercicio de enum e composicao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Composicao1.Entities;
+using Composicao1.Entities.Enums;
+using System;
 
 namespace Composicao1
 {
@@ -6,7 +8,15 @@ namespace Composicao1
     {
         static void Main(string[] args)
         {
-          
+            Worker trabalhador = new Worker();
+            Console.Write("Enter department's name: ");
+            Department departamento = new Department(Console.ReadLine());
+            Console.WriteLine("(=== Enter worker data ===)");
+            Console.Write("Name: ");
+            trabalhador.Name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            trabalhador.Level =Enum.Parse<WorkerLevel>(Console.ReadLine());
+
         }
     }
 }
